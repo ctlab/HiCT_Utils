adb24f03738c1dcb906068ee00ce2600cdb97606
Alexander Serdyukov <AxisAlexNT@users.noreply.github.com>
2022-09-04 20:30:05 +0300

    Delete loadfromsource.ps1

--- loadfromsource.ps1
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-& c:/Users/tux/Documents/HiCT/hict.venv/Scripts/Activate.ps1
-Set-Variable "HICT_DIR" "${PSScriptRoot}/../hict/"
-$env:PYTHONPATH += ";${HICT_DIR}"
-echo "Setting HICT_DIR = ${HICT_DIR} and PYTHONPATH = ${env:PYTHONPATH}"
