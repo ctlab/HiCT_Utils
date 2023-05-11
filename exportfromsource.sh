9e2e480bb94e8f5677684dfd1a6df95367875b72
Aleksandr Serdiukov <ntwwwnt@gmail.com>
2023-05-11 03:24:24 +0300

    Added extsort mode

--- exportfromsource.sh
+++ exportfromsource.sh
@@ -3,6 +3,8 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 HICT_DIR="$SCRIPT_DIR/../HiCT_Library/"
 export PYTHONPATH="$PYTHONPATH:$HICT_DIR"
 echo "Setting PYTHONPATH=$PYTHONPATH"
-# python -m hict_utils export "${HICT_DIR}/../HiCT_Server/data/mat18_100k.mcool.hict.hdf5"
-python -m hict_utils export --agp "${HICT_DIR}/../HiCT_Server/data/g3_scaffolds_final.agp" "${HICT_DIR}/../HiCT_Server/data/g3_4DN.mcool.hict.hdf5"
+{ time python -m hict_utils export "${HICT_DIR}/../HiCT_Server/data/mat18_100k.mcool.hict.hdf5" ; } 2> mat18_single.log
+{ time python -m hict_utils export --ext-sort "${HICT_DIR}/../HiCT_Server/data/mat18_100k.mcool.hict.hdf5" ; } 2> mat18_extsort.log
+{ time python -m hict_utils export --agp "${HICT_DIR}/../HiCT_Server/data/g3_scaffolds_final.agp" "${HICT_DIR}/../HiCT_Server/data/g3_4DN.mcool.hict.hdf5"; } 2> g3_single.log
+{ time python -m hict_utils export --ext-sort --agp "${HICT_DIR}/../HiCT_Server/data/g3_scaffolds_final.agp" "${HICT_DIR}/../HiCT_Server/data/g3_4DN.mcool.hict.hdf5"; } 2> g3_extsort.log
 # python -m hict_utils export "${HICT_DIR}/../HiCT_Server/data/zanu_male_4DN.mcool.hict.hdf5"
--- hict_utils/hict_to_cool/export_to_cooler.py
+++ hict_utils/hict_to_cool/export_to_cooler.py
@@ -30,7 +30,25 @@ from hict.core.common import QueryLengthUnit, ScaffoldDescriptor
 from hict.core.scaffold_tree import ScaffoldTree
 
 import cooler
+import io
+import csv
 
+class CSVSerializer(ext_sort.Serializer):
+
+    def __init__(self, writer):
+        super().__init__(csv.writer(io.TextIOWrapper(writer, write_through=True)))
+
+    def write(self, item):
+        return self._writer.writerow(item)
+
+
+class CSVDeserializer(ext_sort.Deserializer):
+
+    def __init__(self, reader):
+        super().__init__(csv.reader(io.TextIOWrapper(reader)))
+
+    def read(self):
+        return next(self._reader)
 
 class HiCTToCoolerConverter(object):
 
@@ -167,6 +185,176 @@ class HiCTToCoolerConverter(object):
                 ordered=False,
                 h5opts=ds_args
             )
+            
+    def convert_extsort(
+        self,
+        resolutions: Optional[List[int]] = None,
+        maximum_fetch_size_bytes: int = 256*1024*1024*8
+    ):
+        global Ser, DeSer
+        if resolutions is None:
+            resolutions = [min(self.chunked_file.resolutions)]
+
+        ds_args = {
+            "compression": "gzip",
+            "shuffle": True,
+            "chunks": True,
+        }
+
+        # chroms:
+        _, scaffolds = ContactMatrixFacet.get_assembly_info(self.chunked_file)
+        scaffold_count: np.int64 = np.int64(len(scaffolds))
+        scaffold_length_bp = np.array(
+            tuple(map(lambda t: t[1], scaffolds)), dtype=np.int64)
+
+        unnamed_count: int = 0
+
+        def get_name(sd: Optional[ScaffoldDescriptor]) -> str:
+            nonlocal unnamed_count
+            if sd is not None:
+                return sd.scaffold_name
+            else:
+                unnamed_count += 1
+                return f"unscaffolded_contig_region_{unnamed_count}"
+
+        scaffold_names_map = map(get_name, map(lambda t: t[0], scaffolds))
+        scaffold_names_nd = np.array(tuple(scaffold_names_map))
+        # maximum_length = max(map(len, scaffold_names_map))
+
+        for resolution in resolutions:
+            # bins:
+            # bins/chrom:
+            scaffold_length_bp_prefix_sum = np.zeros(
+                shape=(1+scaffold_count,), dtype=np.int64)
+            scaffold_length_bins = np.zeros(
+                shape=(scaffold_count,), dtype=np.int64)
+            np.cumsum(scaffold_length_bp,
+                      out=scaffold_length_bp_prefix_sum[1:], dtype=np.int64)
+            for i in range(scaffold_count):
+                es = self.chunked_file.contig_tree.expose_segment(
+                    0,
+                    scaffold_length_bp_prefix_sum[i],
+                    scaffold_length_bp_prefix_sum[1+i],
+                    units=QueryLengthUnit.BASE_PAIRS
+                )
+                scaffold_length_bins[i] = es.segment.get_sizes(
+                )[0][resolution] if es.segment is not None else np.int64(0)
+            scaffold_length_bins_prefix_sum = np.zeros(
+                shape=(1+scaffold_count,), dtype=np.int64)
+            np.cumsum(scaffold_length_bins,
+                      out=scaffold_length_bins_prefix_sum[1:], dtype=np.int64)
+            total_bin_length = scaffold_length_bins_prefix_sum[-1]
+            bin_to_scaffold = np.zeros(
+                shape=(total_bin_length), dtype=np.int32)
+            for scaffold_index in range(len(scaffold_length_bins)):
+                bin_to_scaffold[scaffold_length_bins_prefix_sum[i]:scaffold_length_bins_prefix_sum[1+i]] = scaffold_index
+            # /resolutions/1000/bins/start:
+            # /resolutions/1000/bins/end:
+            bins_start = np.zeros(shape=(total_bin_length,), dtype=np.int32)
+            bins_end = np.zeros(shape=(total_bin_length,), dtype=np.int32)
+            for scaffold_index in range(scaffold_count):
+                bin_ords = np.arange(
+                    scaffold_length_bins[scaffold_index], dtype=np.int64)
+                bins_start[scaffold_length_bins_prefix_sum[scaffold_index]:scaffold_length_bins_prefix_sum[1+scaffold_index]] = np.int32(resolution*bin_ords)
+                bins_end[scaffold_length_bins_prefix_sum[scaffold_index]:scaffold_length_bins_prefix_sum[1+scaffold_index]] = np.int32(resolution*(1+bin_ords))
+                bins_end[scaffold_length_bins_prefix_sum[1+scaffold_index]-1
+                         ] = np.int32(scaffold_length_bp[scaffold_index])
+
+            bins = pd.DataFrame.from_dict(
+                {'chrom': scaffold_names_nd[bin_to_scaffold], 'start': bins_start, 'end': bins_end})
+
+            val_dtype = self.chunked_file.dtype
+            val_dtype_width_bytes: int = int(
+                np.dtype(val_dtype).itemsize) if val_dtype is not None else 8
+            number_of_blocks_to_fetch: int = int(max(1, maximum_fetch_size_bytes // (
+                val_dtype_width_bytes * self.chunked_file.dense_submatrix_size[resolution] * self.chunked_file.dense_submatrix_size[resolution])))
+
+            row_dtype = np.int32
+            row_dtype_width_bytes: int = int(np.dtype(row_dtype).itemsize) if row_dtype is not None else 4
+            col_dtype = np.int32
+            col_dtype_width_bytes: int = int(np.dtype(col_dtype).itemsize) if col_dtype is not None else 4
+            
+
+            with tempfile.NamedTemporaryFile("w+b") as tmpf:
+                with bz2.open(tmpf, mode="wt", compresslevel=5) as cstream:            
+                    for start_row_incl in range(0, total_bin_length, self.chunked_file.dense_submatrix_size[resolution]):
+                        end_row_excl = min(
+                            start_row_incl + self.chunked_file.dense_submatrix_size[resolution], total_bin_length)
+                        for start_col_incl in range(start_row_incl, total_bin_length, self.chunked_file.dense_submatrix_size[resolution] * number_of_blocks_to_fetch):
+                            end_col_excl = min(
+                                start_col_incl + self.chunked_file.dense_submatrix_size[resolution] * number_of_blocks_to_fetch, total_bin_length)
+                            dense, _, _ = self.chunked_file.get_submatrix(
+                                resolution,
+                                start_row_incl,
+                                start_col_incl,
+                                end_row_excl,
+                                end_col_excl,
+                                exclude_hidden_contigs=False
+                            )
+                            if start_row_incl == start_col_incl:
+                                dense = np.triu(dense)
+                            sparse = coo_array(dense, dtype=(
+                                self.chunked_file.dtype if self.chunked_file.dtype is not None else np.int32))
+                            rows = sparse.row + start_row_incl
+                            cols = sparse.col + start_col_incl
+                            
+                            coo_record_row = np.rec.array(rows, dtype=[('bin1_id', row_dtype)])
+                            coo_record_rcv = npr.append_fields(coo_record_row, data=[cols, sparse.data], names=['bin2_id', 'count'], dtypes=[col_dtype, val_dtype], usemask=False, asrecarray=True)
+                            df = pd.DataFrame(coo_record_rcv)
+                            df.to_csv(cstream, sep=',', header=None, index=None)
+                            del coo_record_rcv
+                            del coo_record_row
+                            del sparse                            
+                            
+                            print(f"Exporting raw pixeltable: {float(start_row_incl*total_bin_length + start_col_incl) / float(total_bin_length*total_bin_length)} Time: {datetime.now().strftime('%H:%M:%S')}", flush=True)
+                            
+                    print(f"Exported raw pixeltable, preparing to sort. Time: {datetime.now().strftime('%H:%M:%S')}", flush=True)
+                    
+                tmpf.seek(0)
+                with bz2.open(tmpf, mode="rt") as cstream:                       
+                    
+                    
+                    with tempfile.NamedTemporaryFile("w+b") as tmpres:
+                        with bz2.open(tmpres, mode="wb", compresslevel=5) as cresstream:
+                            with bz2.open(tmpf, mode="rb") as cstream:
+                                print(f"Launching external sorting. Time: {datetime.now().strftime('%H:%M:%S')}", flush=True)
+                                ext_sort.sort(
+                                    cstream,
+                                    cresstream,
+                                    Serializer=CSVSerializer,
+                                    Deserializer=CSVDeserializer,
+                                    chunk_size=(maximum_fetch_size_bytes // val_dtype_width_bytes),
+                                    workers_cnt=os.cpu_count()
+                                )
+                                print(f"External sorting finished. Time: {datetime.now().strftime('%H:%M:%S')}", flush=True)
+                        tmpres.seek(0)
+                        with bz2.open(tmpres, mode="rt") as cresstream:
+                            # Copy from compressed temporary file to Cooler datasets                            
+                            def fetch_chunk():
+                                #csv.reader(io.TextIOWrapper(cresstream))
+                                buf = cresstream.readlines((maximum_fetch_size_bytes // val_dtype_width_bytes))
+                                if buf:
+                                    df = pd.read_csv(io.StringIO("".join(buf)), sep=',', header=None, index_col=False)
+                                    rows = df.iloc[:, 0].values.astype(row_dtype)
+                                    cols = df.iloc[:, 1].values.astype(col_dtype)
+                                    vals = df.iloc[:, 2].values.astype(val_dtype)
+                                    yield {
+                                        'bin1_id': rows,
+                                        'bin2_id': cols,
+                                        'count': vals
+                                    }
+                                    
+                            print(f"Starting cooler creation. Time: {datetime.now().strftime('%H:%M:%S')}", flush=True)
+
+                            cooler.create_cooler(
+                                cool_uri=f"{str(self.output_file_path.absolute())}::/resolutions/{resolution}",
+                                bins=bins,
+                                pixels=fetch_chunk(),
+                                symmetric_upper=True,
+                                ensure_sorted=True,
+                                ordered=True,
+                                h5opts=ds_args
+                            )
 
 
 def main(cmdline: Optional[List[Any]]):
@@ -194,6 +382,8 @@ def main(cmdline: Optional[List[Any]]):
                         type=int, help="Select resolutions that should be converted (if not specified, converts only the most high-resolution one)", dest="resolutions")
     parser.add_argument("-a", "--agp",
                         type=lambda f: agp_file_checker(parser, f), help="AGP file that should be applied to hict.hdf5 before export", dest="agp")
+    parser.add_argument("-e", "--ext-sort", action="store_true",
+                        help="Use multithreaded ext-sort module instead of single-threaded Cooler implementation (might require significantly more disk space)", dest="ext_sort")
     parser.add_argument("input", help="Input file path",
                         type=lambda f: cool_file_checker(parser, f))
     parser.add_argument(
@@ -226,11 +416,19 @@ def main(cmdline: Optional[List[Any]]):
             ).absolute()
         )
 
-        converter.convert(
-            resolutions=[
-                int(r)
-                for r in args.resolutions
-            ] if args.resolutions is not None else None,
-        )
+        if not args.ext_sort:
+            converter.convert(
+                resolutions=[
+                    int(r)
+                    for r in args.resolutions
+                ] if args.resolutions is not None else None,
+            )
+        else:
+            converter.convert_extsort(
+                resolutions=[
+                    int(r)
+                    for r in args.resolutions
+                ] if args.resolutions is not None else None,
+            )
     finally:
         del chunked_file
